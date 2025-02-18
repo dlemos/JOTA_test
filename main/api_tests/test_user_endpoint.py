35335c71b5f969331e242f042f5f1f3b959fc5a3
Daniel Lemos <xspager+dlemos@gmail.com>
2025-02-18 17:25:54 -0300

    Fix issues pointed out by flake8

--- main/api_tests/test_user_endpoint.py
+++ main/api_tests/test_user_endpoint.py
@@ -1,16 +1,10 @@
-import tempfile
-import pathlib
-from PIL import Image
-
 import pytest
-
-from django.core.files.base import File as DjangoFile
 
 from rest_framework.test import APIClient
 from rest_framework import status
 
 from main.models import User
-from news.models import Category, News
+from news.models import Category
 
 
 @pytest.fixture
@@ -40,16 +34,16 @@ def category():
 
 
 def test_unlogged_user_should_not_be_able_to_access_the_user_endpoint(unlogged_client):
-    response = unlogged_client.get(f"/user/")
+    response = unlogged_client.get("/user/")
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 @pytest.mark.django_db
 def test_regular_user_should_not_be_able_to_access_the_user_endpoint(user_client):
-    response = user_client.get(f"/user/")
+    response = user_client.get("/user/")
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 def test_admin_user_should_be_able_to_access_the_user_endpoint(admin_client):
-    response = admin_client.get(f"/user/")
+    response = admin_client.get("/user/")
     assert response.status_code == status.HTTP_200_OK
